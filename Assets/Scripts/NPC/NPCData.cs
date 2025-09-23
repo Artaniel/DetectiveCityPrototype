4da9887111f7a6838cf2939712b36854d211c025
Artaniel <59449523+Artaniel@users.noreply.github.com>
2025-09-23 23:14:18 +0300

    traits dict and arrays

--- Assets/Scripts/NPC/NPCData.cs
+++ Assets/Scripts/NPC/NPCData.cs
@@ -13,15 +13,30 @@ namespace Assets.Scripts.NPC
 
         public string characterName;
         public Location homeLocation;
-        public Dictionary<Trait, float> traits = new();
 
         public Location workLocation;
         public float workStartTime;
         public float workEndTime;
+        
+        public Trait[] traitsArray;
+        public float[] traitValues;
+        public Dictionary<Trait, float> traits;
 
         public void Init(Boot boot, Npc npc) {
             _boot = boot;
             _npc = npc;
+            FormTraitsDictionary();
+        }
+
+        private void FormTraitsDictionary(){
+            traits = new();
+            for (int i = 0; i < traitsArray.Length; i++) {
+                if (traitValues.Length >= i)
+                    traits.Add(traitsArray[i], traitValues[i]);
+                else                    
+                    traits.Add(traitsArray[i], 1f);
+            }
+
         }
     }
 } 
