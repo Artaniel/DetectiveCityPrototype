78bb7f486ad31029b6334ae293ec3c2b18f7d5d8
Artaniel <59449523+Artaniel@users.noreply.github.com>
2025-09-24 22:28:13 +0300

    steal action

---
+++ Assets/Scripts/NPC/NpcAction/Steal.cs
@@ -0,0 +1,45 @@
+using Assets.Scripts.NPC;
+using Assets.Scripts.Worlds;
+using UnityEngine;
+
+namespace Assets.Scripts.NPC.NpcAction
+{
+    public class Steal : MonoBehaviour, INpcAction
+    {
+        private Boot _boot;
+        private AiSystem _ai;
+        public Trait greed;
+        public Traot criminalTrait;
+        
+        public void Init(Boot boot, AiSystem ai) {
+            _ai = ai;
+            _boot = boot;
+        }
+
+        public bool CanPerform(Npc npc) {
+            return npc.state.currentLocation != npc.data.homeLocation;
+        }
+
+        public float GetUtility(Npc npc) {
+            return npc.data.traits[greed] - npc.data.traits[criminalTrait];
+        }
+
+        public void Execute(Npc npc) {
+            npc.state.isActionComplete = true;
+            npc.state.currentActivity = "Stealing";
+            npc.data.traits[criminalTrait] = 1.0f;
+            Debug.Log($"[{_boot.world.state.currentTimeInMinutes}] {npc.data.characterName}: Steal an item!");    
+        }
+
+        public void TickUpdate(float deltaTime, Npc npc) {
+        }
+
+        public bool IsComplete(Npc npc) {
+            return true;
+        }
+
+        public Location GetRequiredLocation(Npc npc) {
+            return null;
+        }
+    }
+}
