ea342fd5cf2640db0ffa960cd3f8f88a215bc17d
Artaniel <59449523+Artaniel@users.noreply.github.com>
2025-10-08 22:12:14 +0300

    Add NPC selection and list refresh to UI

--- Assets/Scripts/UI/CharSheetUi.cs
+++ Assets/Scripts/UI/CharSheetUi.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using Assets.Scripts;
+using Assets.Scripts.NPC;
 
 public class CharSheetUi : MonoBehaviour
 {    
@@ -8,5 +9,10 @@ public class CharSheetUi : MonoBehaviour
     public void Init(Boot boot, DebugToolsUi debugToolsUi) {
         _boot = boot;
         _debugToolsUi = debugToolsUi;
+    }
+    
+    public void SelectNpc(Npc npc) {
+        Debug.Log("Selected NPC: " + npc.name);
+        // Update UI elements to show NPC details
     }
 }
--- Assets/Scripts/UI/NpcListUi.cs
+++ Assets/Scripts/UI/NpcListUi.cs
@@ -1,12 +1,31 @@
 using UnityEngine;
 using Assets.Scripts;
+using UnityEngine.UI;
+using Assets.Scripts.NPC;
 
 public class NpcListUi : MonoBehaviour
 {
     private Boot _boot;
     private DebugToolsUi _debugToolsUi;
+    public GameObject npcButtonPrefab;
+    public Transform npcListContainer;
+
     public void Init(Boot boot, DebugToolsUi debugToolsUi) {
         _boot = boot;
         _debugToolsUi = debugToolsUi;
+    }
+    
+    public void RefreshNpcList() {
+        foreach (Transform child in npcListContainer) {
+            Destroy(child.gameObject);
+        }
+
+        foreach (Npc npc in _boot.world.state.npcs) {
+            GameObject buttonObj = Instantiate(npcButtonPrefab, npcListContainer);
+            Button button = buttonObj.GetComponent<Button>();
+            button.onClick.AddListener(() => {
+                _debugToolsUi.charSheetUi.SelectNpc(npc);
+            });
+        }
     }
 }
