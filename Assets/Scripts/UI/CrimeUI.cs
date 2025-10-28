3129b41a95887753bb01d170ac6e4e1513ce3fce
Artaniel <59449523+Artaniel@users.noreply.github.com>
2025-10-28 23:47:34 +0300

    CrimeUi

---
+++ Assets/Scripts/UI/CrimeUI.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using Assets.Scripts;
+using Assets.Scripts.NPC;
+using Assets.Scripts.NPC.Traits;
+using Assets.Scripts.Items;
+using TMPro;
+
+public class CrimeUi : MonoBehaviour
+{
+    private Boot _boot;
+    private DebugToolsUi _debugToolsUi;
+
+    public void Init(Boot boot, DebugToolsUi debugToolsUi) {
+        _boot = boot;
+        _debugToolsUi = debugToolsUi;
+    }
+
+    public void SelectNpc(Npc npc) {
+        
+    }
+}
